e5713f1d9e6d033d9698f0ad7900c10b1e04e89e
Mr. Fa <133749021+Mr-Soap@users.noreply.github.com>
2026-05-04 14:12:36 +0700

    arsitektur clean

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:utd_store/features/product/presentation/pages/splash_screen.dart';
+import 'core/theme/app_theme.dart';
+import 'core/di/injector.dart';
+void main() {
+  runApp(const MainApp());
+}
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'UTD STORE - Syifa',
-      home: Scaffold(
-        body: Center(
-          child: Text('data'),
-        ),
-      ),
+      theme: AppTheme.lightTheme,
+      home: const SplashScreen()
     );
   }
 }
